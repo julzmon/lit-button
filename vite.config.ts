28aba907707c629c17eaec0dc677c2949c58d8a6
Julian <julzmon@gmail.com>
2025-05-28 18:18:38 +0000

    vite config for codesanbox

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,3 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
-export default defineConfig({
-  server: {
-    port: 3000,
-    open: true
-  }
-});
+export default defineConfig({});
